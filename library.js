85f49071ecdac585abf56f2f705ae9e660ce7bc0
am1cable <tegan.mcfa@gmail.com>
2018-10-28 11:29:02 -0400

    improving regex speed

--- library.js
+++ library.js
@@ -13,10 +13,9 @@ module.exports.composerFormatting = composerFormatting;
 
 const replaceSpoilerContent = (content) => {
 	var newContent = content;
-	const startSpoiler = new RegExp (/(?:(?<!<code[\s\S]*)|(?<=<\/code[\s\S]*))\[s(?:=([^\]]*)|)\]/gm);
-	const endSpoiler = new RegExp (/(?:(?<!<code[\s\S]*)|(?<=<\/code[\s\S]*))\[\/s\](<\/li>\n<\/(?:ol|ul)>|)/gm);
-
-	newContent = newContent.replace(startSpoiler, (match, p1) => `<section class="spoiler-wrapper"><button class="spoiler-control">${p1 ? p1 : 'Spoiler'}</button><section style="display: none;" class="spoiler-content">`);
+	const startSpoiler = new RegExp (/(?:(?<!<code>)|(?<=<\/code>))\[s(?:=([^\]]*)|)\]/g);
+	const endSpoiler = new RegExp (/(?:(?<!<code>)|(?<=<\/code>))\[\/s\](<\/li>\n<\/(?:ol|ul)>|)/g);
+	newContent = newContent.replace(startSpoiler, (match, p1) => `<section class="spoiler-wrapper"><button class="spoiler-control btn btn-default">${p1 ? p1 : 'Spoiler'}</button><section style="display: none;" class="spoiler-content">`);
 	newContent = newContent.replace(endSpoiler, (match, p1) => `${p1}</section></section>`);
 	return newContent;
 }
