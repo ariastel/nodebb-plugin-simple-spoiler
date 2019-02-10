6dbb7741b11b11659db8800a933979e9d0953eda
am1cable <tegan.mcfa@gmail.com>
2019-02-10 14:26:00 -0500

    actually working with code blocks

--- library.js
+++ library.js
@@ -13,11 +13,23 @@ module.exports.composerFormatting = composerFormatting;
 
 const replaceSpoilerContent = (content) => {
 	var newContent = content;
-	const startSpoiler = new RegExp (/(?:(?<!<code>)|(?<=<\/code>))\[s(?:=([^\]]*)|)\]/g);
-	const endSpoiler = new RegExp (/(?:(?<!<code>)|(?<=<\/code>))\[\/s\](<\/li>\n<\/(?:ol|ul)>|)/g);
-	newContent = newContent.replace(startSpoiler, (match, p1) => `<section class="spoiler-wrapper"><button class="spoiler-control btn btn-default">${p1 ? p1 : 'Spoiler'}</button><section style="display: none;" class="spoiler-content">`);
-	newContent = newContent.replace(endSpoiler, (match, p1) => `${p1}</section></section>`);
+	console.log(newContent);
+	if (newContent.includes('<code')) {
+		const separatedByCodeBlocks = newContent.replace(/<code/gi, "splitme<code").replace(/<\/code>/gi, "<\/code>splitme").split("splitme");
+		const formattedBlocks = separatedByCodeBlocks.map(text => text.includes('<code') ? text : formatSpoiler(text));
+		newContent = formattedBlocks.join('');
+	}
+	else{
+		newContent = formatSpoiler(newContent);
+	}
+	console.log(newContent);
 	return newContent;
+}
+
+const formatSpoiler = (text) => {
+	const startSpoiler = new RegExp(/\[s(?:=([^\]]*)|)\]/g);
+	const endSpoiler = new RegExp(/\[\/s\](<\/li>\n<\/(?:ol|ul)>|)/g);
+	return text.replace(startSpoiler, (match, p1) => `<section class="spoiler-wrapper"><button class="spoiler-control btn btn-default">${p1 ? p1 : 'Spoiler'}</button><section style="display: none;" class="spoiler-content">`).replace(endSpoiler, (match, p1) => `${p1}</section></section>`);
 }
 
 const parsePost = (data, callback) => {
