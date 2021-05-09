c4fdaf15459f92bd742ed533ebc6aecfcba9399b
Antosik <grig_anton96@mail.ru>
2021-05-09 15:54:04 +0300

    feat: fork

--- library.js
+++ library.js
@@ -1,45 +1,45 @@
-
 'use strict';
 
-const composerFormatting = (data, callback) => {
-	data.options.push({
-		name: 'spoiler',
-		className: 'fa fa-eye',
-		title: 'Spoiler',
-	});
-	callback(null, data);
-};
-module.exports.composerFormatting = composerFormatting;
+const SpoilerPlugin = {};
 
-const replaceSpoilerContent = (content) => {
-	var newContent = content;
+function formatSpoiler(text) {
+	const startSpoiler = new RegExp(/\[s(?:=([^\]]*)|)\]/g);
+	const endSpoiler = new RegExp(/\[\/s\](<\/li>\n<\/(?:ol|ul)>|)/g);
+	return text.replace(
+		startSpoiler, 
+		(match, p1) => `<section class="spoiler-wrapper"><button class="spoiler-control btn btn-default">${p1 ? p1 : 'Spoiler'}</button><section style="display: none;" class="spoiler-content">`)
+			.replace(endSpoiler, (match, p1) => `${p1}</section></section>`
+	);
+}
+function replaceSpoilerContent(content) {
+	let newContent = content;
 	if (newContent.includes('<code')) {
 		const separatedByCodeBlocks = newContent.replace(/<code/gi, "splitme<code").replace(/<\/code>/gi, "<\/code>splitme").split("splitme");
 		const formattedBlocks = separatedByCodeBlocks.map(text => text.includes('<code') ? text : formatSpoiler(text));
 		newContent = formattedBlocks.join('');
-	}
-	else{
+	} else {
 		newContent = formatSpoiler(newContent);
 	}
 	return newContent;
 }
 
-const formatSpoiler = (text) => {
-	const startSpoiler = new RegExp(/\[s(?:=([^\]]*)|)\]/g);
-	const endSpoiler = new RegExp(/\[\/s\](<\/li>\n<\/(?:ol|ul)>|)/g);
-	return text.replace(startSpoiler, (match, p1) => `<section class="spoiler-wrapper"><button class="spoiler-control btn btn-default">${p1 ? p1 : 'Spoiler'}</button><section style="display: none;" class="spoiler-content">`).replace(endSpoiler, (match, p1) => `${p1}</section></section>`);
-}
+SpoilerPlugin.composerFormatting = async function composerFormatting(data) {
+	data.options.push({
+		name: 'spoiler',
+		className: 'fa fa-eye',
+		title: '[[aa_simple-spoiler:spoiler]]',
+	});
+	return data;
+};
 
-const parsePost = (data, callback) => {
-	var newContent = replaceSpoilerContent(data.postData.content);
+SpoilerPlugin.parsePost = async function parsePost(data) {
 	const newData = { ...data };
-	newData.postData.content = newContent;
-	callback(null, newData);
+	newData.postData.content = replaceSpoilerContent(data.postData.content);
+	return newData;
 };
-module.exports.parsePost = parsePost;
 
-const parseRaw = (data, callback) => {
-	var newContent = replaceSpoilerContent(data);
-	callback(null, newContent);
+SpoilerPlugin.parseRaw = async function parseRaw(data) {
+	return replaceSpoilerContent(data);
 };
-module.exports.parseRaw = parseRaw;
+
+module.exports = SpoilerPlugin;
